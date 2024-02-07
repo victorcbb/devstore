f46e29784d75cabc9db2256399c0ac5449363d31
Victor <victorclovisb@hotmail.com>
2024-02-06 21:09:02 -0300

    Contexto de adicionar itens no carrinho

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,11 +1,14 @@
 import { ReactNode } from 'react'
 import { Header } from '@/components/header'
+import { CartProvider } from '@/context/cart-context'
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
-    <main className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-8">
-      <Header />
-      {children}
-    </main>
+    <CartProvider>
+      <main className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-8">
+        <Header />
+        {children}
+      </main>
+    </CartProvider>
   )
 }
